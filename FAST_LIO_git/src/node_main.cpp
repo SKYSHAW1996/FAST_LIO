4bda47dc8313994f54112a3b87ba1a312359d778
SKYSHAW1996 <skxiao15@163.com>
2024-03-25 14:19:09 +0800

    DIY_v1_20230325

---
+++ FAST_LIO_git/src/node_main.cpp
@@ -0,0 +1,23 @@
+#include <Eigen/Core>
+
+#include <so3_math.h>
+#include <common_lib.h>
+#include "IMU_Processing.hpp"
+#include "preprocess.h"
+#include <ikd-Tree/ikd_Tree.h>
+#include "laserMapping.h"
+
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "laserMapping");
+    ros::NodeHandle nh;
+
+    ROS_INFO("Let's Start FAST-LIO.");
+    // fast_lio::FastLIO fast_lio_mapping(nh);
+    std::shared_ptr<fast_lio::FastLIO> fast_lio_instance = fast_lio::FastLIO::getFastLIO(nh);
+    ros::spin();
+
+    return 0;
+}
